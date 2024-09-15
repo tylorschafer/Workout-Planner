bb9cfbb18d4955aca4323e9abd777c442b25d8f8
Tylor Schafer <tylorschafer@gmail.com>
2024-09-15 12:27:54 -0600

    Initial Commit

---
+++ Workout PlannerTests/Workout_PlannerTests.swift
@@ -0,0 +1,16 @@
+//
+//  Workout_PlannerTests.swift
+//  Workout PlannerTests
+//
+//  Created by Tylor Schafer on 9/15/24.
+//
+
+import Testing
+
+struct Workout_PlannerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
